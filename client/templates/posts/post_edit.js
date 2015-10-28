fc02afcb7e9ba99090b610d8c3168fc4ab19dada
Andre Liem <andre.liem@gmail.com>
2015-10-28 10:58:53 -0700

    updated

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,45 @@
+Template.postEdit.events({
+	'submit form': function(e) {
+		e.preventDefault();
+
+		var currentPostId = this._id;
+
+		var postProperties = {
+			url: $(e.target).find('[name=url]').val(),
+			title: $(e.target).find('[name=title]').val()
+		};
+
+		/*
+		Posts.update(currentPostId, {$set: postProperties}, function(error) {
+			if (error) {
+				alert(error.reason);
+			} else {
+				Router.go('postPage', {_id: currentPostId});
+			}
+		});
+		*/
+
+		Meteor.call('postUpdate', currentPostId, postProperties, function(error, result) {
+			if (error)
+				return alert(error.reason);
+
+			if (result.postExists) 
+				alert('This link has already been posted.');
+
+			Router.go('postPage', {_id: result._id});
+		});
+
+
+	},
+
+	'click .delete': function(e) {
+		e.preventDefault();	
+
+		if (confirm("Delete this post?")) {
+			var currentPostId = this._id;
+			Posts.remove(currentPostId);
+			Router.go('postsList');
+		}
+	}
+
+});
